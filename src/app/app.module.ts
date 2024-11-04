133233eabf5ad43f5a7e29bf4891cdf784f415de
Ikram-Khan70 <mdikramkhan70@gmail.com>
2024-11-04 11:07:35 +0530

    new

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,3 @@
-
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
@@ -6,18 +5,26 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { SigninComponent } from './signin/signin.component';
-import { RegistrationComponent } from './registration/registration.component';
+// import { RegistrationComponent } from './registration/registration.component';
 import { MatStep, MatStepper, MatStepperModule } from '@angular/material/stepper';
 import { MatInput, MatInputModule } from '@angular/material/input';
 import { MatButton, MatButtonModule } from '@angular/material/button';
 import { MatCalendar } from '@angular/material/datepicker';
 import { MatCardModule } from '@angular/material/card';
+import { RegistrationComponent } from './registration/registration.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { FooterComponent } from './footer/footer.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     SigninComponent,
-    RegistrationComponent
+    RegistrationComponent,
+    RegistrationComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
@@ -27,7 +34,8 @@ import { MatCardModule } from '@angular/material/card';
     MatStepperModule,
     MatInputModule,
     MatButtonModule,
-    MatCardModule
+    MatCardModule,
+    MatToolbarModule
   ],
   providers: [
     provideAnimationsAsync()
@@ -35,4 +43,3 @@ import { MatCardModule } from '@angular/material/card';
   bootstrap: [AppComponent]
 })
 export class AppModule { }
-
